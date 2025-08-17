ce3c8ed7c4436d2bea811c4be2dfdc255c54b033
ND <101196401+Nandan195@users.noreply.github.com>
2025-08-17 21:29:36 +0530

    Add .next/static/chunks/src_app_dashboard_page_tsx_4cc37a58._.js 1755446376118

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_4cc37a58._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8b369af7._.js",
+  "static/chunks/node_modules_3699f839._.js"
+],
+    source: "dynamic"
+});
